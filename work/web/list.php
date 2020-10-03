d3cc33c45b54a43c10e1d345094e7a94333f8d7a
Ahiram <ahiram.seagull@gmail.com>
2020-10-03 18:38:53 +0900

    add little change

--- work/web/list.php
+++ work/web/list.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   
   validateToken();
   $ids=filter_input(INPUT_POST, 'ids', FILTER_DEFAULT,FILTER_REQUIRE_ARRAY);
-  if (empty($ids) ===True);
+  if (empty($ids) === True);
     header('Location: http://localhost:8080/web/list.php');
     exit;
   $newdata=[];
@@ -34,22 +34,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 createToken()
 
 ?>
-      <div>
+      <div id=lists>
         <h1>単語リスト</h1>
-        <form action="" method="POST">
           <ul>
           <?php 
+          $oplines=[];
           if (($fp = fopen(FILENAME, "r")) !== false || count(file(FILENAME)) !==0){
             while (($line = fgetcsv($fp, 1000, "|")) !== false) {
+              $oplines[]='<option value="'.$line[0].'">'.$line[0].'</option>';
               ?>
-              <li><label><input type="checkbox" name="ids[]" value="1"><?= $line[0]." | ".$line[1]." | ".$line[2]; ?></label></li>
-            <?php }
+              <li><?= $line[0]."   |   ".$line[1]."   |   ".$line[2]; ?></li>
+            <?php
+            }
           fclose($fp);
           }
           ?>
+          </ul>
+        <form action="" method="POST">
+          <select name="ids[]" multiple id="selectbox">
+          <?php
+          foreach($oplines as $opline){
+            echo $opline;
+            echo "<br>";
+          }
+          ?>
+          </select>
           <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
           <button>delete</button>
-          </ul>
         </form>
         <?= var_dump($ids) ?>
       </div>
